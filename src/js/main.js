d4d3a185d8746412565ac7e6b7044d1bb6b9fed5
Valeria N <v.567insp@gmail.com>
2024-11-04 12:19:45 +0200

    header

---
+++ src/js/main.js
@@ -0,0 +1,36 @@
+import '../scss/style.scss'
+// import switchThemeColor from './localstaradge.js'
+// switchThemeColor()
+
+// burger
+(function () {
+	const isOpen = document.querySelector(".header__inner-burger")
+	const isClose = document.querySelector(".header__nav-close")
+	const navigation = document.querySelector(".header__nav")
+
+	isOpen.addEventListener('click', () => {
+		navigation.classList.add('active')
+	})
+
+	isClose.addEventListener('click', () => {
+		navigation.classList.remove('active')
+	})
+})();
+
+var swiper = new Swiper(".swiper", {
+    effect: "coverflow",
+    grabCursor: true,
+    centeredSlides: true,
+    slidesPerView: "auto",
+    coverflowEffect: {
+      rotate: 40,
+      stretch: 0,
+      depth: 100,
+      modifier: 1,
+      slideShadows: true,
+    },
+
+    pagination: {
+      el: ".swiper-pagination",
+    },
+});
